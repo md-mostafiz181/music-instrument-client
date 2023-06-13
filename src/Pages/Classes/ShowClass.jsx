024259163577520066f175fa1d20a16e3fc5b09d
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-06-13 20:54:01 +0600

    added instructors and customize

--- src/Pages/Classes/ShowClass.jsx
+++ src/Pages/Classes/ShowClass.jsx
@@ -5,17 +5,18 @@ const ShowClass = ({ approvedCls }) => {
     approvedCls;
   console.log(approvedCls);
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-full bg-base-100 shadow-xl">
       <figure>
         <img src={classImage} alt="" />
 
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Class name:{className}</h2>
-        <p>Price: {price}</p>
-        <p>Available seats: {seats}</p>
-        <p>Total Students: {totalStudent}</p>
-        <p>{instructorName}</p>
+        <h2 className="card-title uppercase font-bold">Class name: {className}</h2>
+        <p className="font-bold">Price: <span className="text-blue-400">${price}</span></p>
+        <p className="font-bold">Available seats: <span className="text-blue-400">{seats}</span></p>
+        <p className="font-bold">Total Students: <span className="text-blue-400">{totalStudent}</span></p>
+        <p className="font-bold">{instructorName}</p>
+        <button className="btn btn-primary text-white">Selected</button>
       </div>
     </div>
   );
