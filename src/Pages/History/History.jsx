4cb147a7e9ffe4c50612b56c9e017a5050ba6b23
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-06-14 11:52:04 +0600

    added readme file

--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -9,7 +9,7 @@ const History = () => {
     <div className=" py-3">
       <h1 className="text-4xl font-bold font-popins text-center">The History</h1>
       <p className="text-xl font-semibold font-popins px-2 text-center mb-4">
-        Who was MUSIC HUNT and for what purpose? There is no room for such silly{" "}
+        What is MUSIC HUNT and for what purpose? There is no room for such silly{" "}
         <br /> questions! We are here for the music
       </p>
 
