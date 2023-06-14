01230d0c1a9f73eb1a859b24533d0b7fb956e042
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-06-15 03:53:07 +0600

    added popular instructor and popular class

--- src/Pages/PopularClass/PopularClass.jsx
+++ src/Pages/PopularClass/PopularClass.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import {  Slide } from 'react-awesome-reveal';
+import React, { useEffect, useState } from "react";
+import { Slide } from "react-awesome-reveal";
 
 const PopularClass = () => {
-    return (
-        <div className='py-10'>
-           <Slide> <h2 className='text-center text-4xl font-bold font-popins uppercase'>Our Popular Class</h2></Slide>
-        </div>
-    );
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/popularClass")
+      .then((res) => res.json())
+      .then((data) => setPopular(data));
+  }, []);
+
+  return (
+    <div className="py-10">
+      <Slide>
+        {" "}
+        <h2 className="text-center text-4xl font-bold font-popins uppercase">
+          Our Popular Class
+        </h2>
+        <p className="font-xl font-semibold font-popins text-center mt-2 mb-3">
+          Its our popular class. Most of the students want <br /> to learn this
+          kind of instrument.
+        </p>
+      </Slide>
+
+      <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-5 ">
+        {popular.map((popular) => (
+          <div className="card card-compact w-full bg-gray-300 shadow-xl">
+            <figure>
+              <img src={popular.classImage} alt="" />
+            </figure>
+            <div className="card-body text-center">
+              <h2 className="text-3xl uppercase font-bold ">
+                {popular.className}
+              </h2>
+              <p className="font-semibold font-popins">
+                {popular.instructorEmail}
+              </p>
+
+              <p className="font-semibold font-popins">
+                Price: ${popular.price}
+              </p>
+              <p className="font-semibold font-popins">Seats:{popular.seats}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularClass;
--- src/Pages/PopularInstructor/PopularInstructor.jsx
+++ src/Pages/PopularInstructor/PopularInstructor.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Slide } from 'react-awesome-reveal';
+import React, { useEffect, useState } from "react";
+import { Slide } from "react-awesome-reveal";
 
 const PopularInstructor = () => {
-    return (
-        <div className='py-10'>
-            <Slide><h1 className='text-center font-bold text-4xl font-popins uppercase '>Popular Instructor</h1></Slide>
-        </div>
-    );
+
+
+  const [instructor, setInstructor] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/instructors")
+      .then((res) => res.json())
+      .then((data) => setInstructor(data));
+  }, []);
+
+
+  return (
+    <div className="py-10 ">
+      <Slide>
+        <h1 className="text-center font-bold text-4xl font-popins uppercase ">
+          Popular Instructor
+        </h1>
+        <p className="text-center font-xl font-semibold font-popins">They are our popular instructor. They are always dedicated <br /> them self for their students.</p>
+      </Slide>
+
+      <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-5 mt-5">
+        {instructor.slice(0, 6).map((ins) => (
+          <div key={ins._id} className="card w-96 bg-slate-300 shadow-xl">
+            <figure>
+              <img
+                src={ins.image}
+                alt=""
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="text-2xl uppercase font-popins  font-semibold text-center">{ins.name}</h2>
+              
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularInstructor;
